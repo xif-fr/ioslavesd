9a987d31164c603ee56858baff99059512583548
Félix Faisant <xcodexif@xif.fr>
2015-07-20 18:06:44 +0200

    generate_random: use /dev/urandom

--- common/common.cpp
+++ common/common.cpp
@@ -168,10 +168,10 @@ std::string ioslaves::md5 (std::string to_hash) {
 }
 
 std::string ioslaves::generate_random (size_t sz) {
-	fd_t fd = ::open("/dev/random", O_RDONLY);
-	if (fd == INVALID_HANDLE) throw xif::sys_error("open(/dev/random) failed");
+	fd_t fd = ::open("/dev/urandom", O_RDONLY);
+	if (fd == INVALID_HANDLE) throw xif::sys_error("open(/dev/urandom) failed");
 	char buf[sz];
-	if (::read(fd, buf, sz) != (ssize_t)sz) throw xif::sys_error("read(/dev/random) failed");
+	if (::read(fd, buf, sz) != (ssize_t)sz) throw xif::sys_error("read(/dev/urandom) failed");
 	return bin_to_hex((unsigned char*)buf, sz);
 }
 
